700f7b463be0032e0e1a8253a085a86f97664303
Hema35 <cheershema@gmail.com>
2021-07-23 18:06:02 -0400

    alternate for ngmodel using binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,21 @@ export class AppComponent {
   title = 'stock-market';
 
   public stock: Stock;
+  private counter: number = 1;
 
   ngOnInit(): void {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    this.stock = new Stock('Test Stock Company - '+ this.counter++, 'TSC', 85, 80);
   }
   onToggleFavorite(stock:Stock){
     console.log('favorite for stock', stock, 'was triggered');
     this.stock.favorite = !this.stock.favorite;
+  }
+
+  changeStockObject() {
+    this.stock = new Stock('Test Stock Company - '+ this.counter++, 'TSC', 85, 80);
+  }
+
+  changeStockPrice() {
+    this.stock.price += 10;
   }
 }
---
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Stock } from 'src/app/model/stock';
+
+@Component({
+  selector: 'app-create-stock',
+  templateUrl: './create-stock.component.html',
+  styleUrls: ['./create-stock.component.scss']
+})
+export class CreateStockComponent  {
+
+public stock: Stock;
+  constructor() {
+    this.stock = new Stock('test','',0,0);
+   }
+
+
+  }
+
+
